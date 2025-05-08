5b604b27d3fb6eff063e443d3e4a3fe97ca6a53f
Sakan Nirattisaykul <sakan.nir@gmail.com>
2025-05-09 03:26:21 +0700

    refactor: update README and layout metadata for clarity on project goals and description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,10 +12,15 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+const TITLE = "Honkai: Star Rail In-App Purchase Analysis";
+const DESCRIPTION = `
+Evaluating the cost-effectiveness of various in-app purchase bundles available in Honkai: Star Rail.
+The goal is to help players make more informed decisions when spending money on in-game currency (shards) used for gacha rolls.
+`;
+
 export const metadata: Metadata = {
-  title: "Honkai: Star Rail In-App Purchase Analysis",
-  description:
-    "Honkai: Star Rail - Comprehensive data analysis of value metrics for in-game purchases",
+  title: TITLE,
+  description: DESCRIPTION,
 };
 
 export default function RootLayout({
